4c43509ec5f52550a959500589b1da928f152bc5
fanischam <fchamalidis@yahoo.com>
2023-08-05 17:39:04 +0300

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Container} from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-        <HomeScreen/>
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,25 +1,23 @@
-import {Card} from 'react-bootstrap'
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Product = ({ product}) => {
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />
-        </a>
-
-        <Card.Body>
-            <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </a>
-
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
-        </Card.Body>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+      to{" "}
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        to <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
