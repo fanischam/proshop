09d564b3c0fd1aa1741934b5f703d26997cd48fc
fanischam <fchamalidis@yahoo.com>
2023-08-06 20:36:01 +0300

    refactoring

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,31 @@
-import {Row, Col} from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
+import { Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Programs</h1>
       <Row>
         {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-            <Product product={product}/>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
